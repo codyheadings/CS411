ed8d05d895437e8e28f88727129eb6dbd6b3bba1
codyheadings <codyheadings05@gmail.com>
2024-10-06 22:42:22 -0400

    adding code files for hw submission

---
+++ wildlife_tracker/migration_tracking/migration_path.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from wildlife_tracker.habitat_management.habitat import Habitat
+
+class MigrationPath:
+
+    def __init__(self,
+                path_id: int,
+                species: str,
+                start_location: Habitat,
+                destination: Habitat,
+                duration: Optional[int] = None) -> None:
+        self.path_id = path_id
+        self.start_location = start_location
+        self.species = species
+        self.destination = destination
+        self.duration = duration
+    
+def get_migration_path_details(path_id) -> dict:
+    pass
+
+def update_migration_path_details(path_id: int, **kwargs) -> None:
+    pass
+
